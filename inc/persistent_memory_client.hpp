3c03e052cdd46948b7fbb0df47ec186e35cbe79f
Ben <ben.quan@vertiq.co>
2022-09-19 10:15:11 -0400

    added format keys to clients in persistent_memory_client

--- inc/persistent_memory_client.hpp
+++ inc/persistent_memory_client.hpp
@@ -25,29 +25,37 @@ class PowerMonitorClient: public ClientAbstract{
     PowerMonitorClient(uint8_t obj_idn):
       ClientAbstract(     kTypePersistentMemory, obj_idn),
       erase_(             kTypePersistentMemory, obj_idn, kSubErase),
-      revert_to_default_( kTypePersistentMemory, obj_idn, kSubRevertToDefault)
+      revert_to_default_( kTypePersistentMemory, obj_idn, kSubRevertToDefault),
+      format_key_1_(      kTypePersistentMemory, obj_idn, kSubFormatKey1),
+      format_key_2_(      kTypePersistentMemory, obj_idn, kSubFormatKey2)
       {};
 
     // Client Entries
     // Control commands
-    ClientEntryVoid   erase_;
-    ClientEntryVoid   revert_to_default_;
+    ClientEntryVoid       erase_;
+    ClientEntryVoid       revert_to_default_;
+    ClientEntry<int32_t>  format_key_1_;
+    ClientEntry<int32_t>  format_key_2_;
 
 
     void ReadMsg(uint8_t* rx_data, uint8_t rx_length)
     {
-      static const uint8_t kEntryLength = kSubRevertToDefault+1;
+      static const uint8_t kEntryLength = kSubFormatKey2+1;
       ClientEntryAbstract* entry_array[kEntryLength] = {
-        &erase_,            // 0
-        &revert_to_default_ // 1
+        &erase_,             // 0
+        &revert_to_default_, // 1
+        &format_key_1_,      // 2
+        &format_key_2_       // 3
       };
 
       ParseMsg(rx_data, rx_length, entry_array, kEntryLength);
     }
 
   private:
-    static const uint8_t kSubErase =            0;
-    static const uint8_t kSubRevertToDefault =  1;
+    static const uint8_t kSubErase            = 0;
+    static const uint8_t kSubRevertToDefault  = 1;
+    static const uint8_t kSubFormatKey1       = 2;
+    static const uint8_t kSubFormatKey2       = 3;
 };
 
 #endif /* PERSISTENT_MEMORY_CLIENT_HPP_ */
