ebfa09a05f6a159d97efb67e2e96c6c6fd984768
Jordan Leiber <jordan.leiber@vertiq.co>
2024-01-26 11:48:53 -0500

    added header comment on PackageIfciCommandsForTransmission. updated formatting of var names

--- inc/iquart_flight_controller_interface_client.hpp
+++ inc/iquart_flight_controller_interface_client.hpp
@@ -50,14 +50,14 @@ class IQUartFlightControllerInterfaceClient : public ClientAbstract {
    public:
     IQUartFlightControllerInterfaceClient(uint8_t obj_idn)
         : ClientAbstract(kTypeIQUartFlightControllerInterface, obj_idn),
-          packed_command(kTypeIQUartFlightControllerInterface, obj_idn, kSubPackedCommand),
+          packed_command_(kTypeIQUartFlightControllerInterface, obj_idn, kSubPackedCommand),
           telemetry_(kTypeIQUartFlightControllerInterface, obj_idn, kSubTelemetry),
           throttle_cvi_(kTypeIQUartFlightControllerInterface, obj_idn, kSubThrottleCvi),
           x_cvi_(kTypeIQUartFlightControllerInterface, obj_idn, kSubXCvi),
           y_cvi_(kTypeIQUartFlightControllerInterface, obj_idn, kSubYCvi){};
 
     // Client Entries
-    PackedClientEntry packed_command;
+    PackedClientEntry packed_command_;
     ClientEntry<IFCITelemetryData> telemetry_;
     ClientEntry<uint8_t> throttle_cvi_;
     ClientEntry<uint8_t> x_cvi_;
@@ -66,7 +66,7 @@ class IQUartFlightControllerInterfaceClient : public ClientAbstract {
     void ReadMsg(uint8_t* rx_data, uint8_t rx_length) {
         static const uint8_t kEntryLength              = kSubYCvi + 1;
         ClientEntryAbstract* entry_array[kEntryLength] = {
-            &packed_command, // 0
+            &packed_command_, // 0
             &telemetry_,     // 1
             &throttle_cvi_,  // 2
             &x_cvi_,         // 3
@@ -75,6 +75,16 @@ class IQUartFlightControllerInterfaceClient : public ClientAbstract {
         ParseMsg(rx_data, rx_length, entry_array, kEntryLength);
     }
 
+    /**
+     * @brief This function takes in an IFCIPackedMessage struct, and prepares the data as a series of bytes ready for transmission over IQUART
+     * As an example, assume you want to send 4 throttle commands, and get telemetry from Module ID 3. To do so, make an IFCIPackedMessage and set
+     * commands[0] through commands[3] to throttle values [0, 65535], telem_byte to 3, and num_cvs to 4. Then, create a byte array, and a byte to store the
+     * message length, and call this function.
+     * 
+     * @param ifci_commands The IFCIPackedMessage struct that you want to send
+     * @param output_data A pointer to an array of bytes that will store the data
+     * @param output_data_length The length of the data stored in the output_data array
+     */
     void PackageIfciCommandsForTransmission(IFCIPackedMessage ifci_commands, uint8_t * output_data, uint8_t * output_data_length){      
       //Copy the CV bytes
       memcpy(output_data, ifci_commands.commands, ifci_commands.num_cvs * 2);
@@ -82,6 +92,7 @@ class IQUartFlightControllerInterfaceClient : public ClientAbstract {
       //Add the telem byte
       output_data[ifci_commands.num_cvs * 2] = ifci_commands.telem_byte;
 
+      //Set the output data length
       *output_data_length = ifci_commands.num_cvs * 2 + 1;
     }
 
